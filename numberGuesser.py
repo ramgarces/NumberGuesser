fe16713dfc0e2f46396bfc7a8d4652ceda1e9306
Ramiro <ramgarces24@gmail.com>
2025-05-18 11:24:59 -0500

    Add while loop to keep random number the same throughout guesses

--- numberGuesser.py
+++ numberGuesser.py
@@ -5,14 +5,15 @@ import random
 # Generate a random number and assign to a variable
 random_number = random.randint(1, 10)
 
-# Get user's guess
+# Get user's guess for the first time
 user_guess = int(input("Guess a number 1 - 10: "))
 
-# Does the user's guess match the random number?
-if user_guess == random_number:
-    print("You did it!")
-else:
+# Keep game going while user's guess is not random number
+while user_guess != random_number:
     print("Guess again.")
-
-print(f"Random number is: {random_number}")
-print(f"User number is: {user_guess}")
+    print(f"(TESTING) Random number: {random_number}")
+    user_guess = int(input("Guess a number 1 - 10: "))
+else:
+    print("You did it!")
+    print("")
+    print(f"The number was {random_number}")
