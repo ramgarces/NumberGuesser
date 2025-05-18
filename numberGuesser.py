5f8832930154ffdf0eb92d78ed0914799ea8b02a
Ramiro <ramgarces24@gmail.com>
2025-05-18 17:50:46 -0500

    Add feature for user to set max range for guesses

--- numberGuesser.py
+++ numberGuesser.py
@@ -1,18 +1,28 @@
-# Ideas: Have user select number range for guessing
+# Ideas: Add a feature to let the user know that they need to go
+#       higher or lower on their guess
 
 import random
 
-# Generate a random number and assign to a variable
-random_number = random.randint(1, 10)
+# Define function to put all game code into
+def play_game():
+    # Set range for number guesses
+    print("Current range: 1 - ___")
+    range_max = int(input("Enter range max: ")) 
 
-# Get user's guess for the first time
-user_guess = int(input("Guess a number 1 - 10: "))
+    # Generate a random number and assign to a variable
+    random_number = random.randint(1, range_max)
 
-# Keep game going while user's guess is not random number
-while user_guess != random_number:
-    print("Guess again.")
-    user_guess = int(input("Guess a number 1 - 10: "))
-else:
-    print("You did it!")
-    print("")
-    print(f"The number was {random_number}")
+    # Get user's guess for the first time
+    user_guess = int(input(f"Guess a number 1 - {range_max}: "))
+
+    # Keep game going while user's guess is not random number
+    while user_guess != random_number:
+        print("Guess again.")
+        user_guess = int(input(f"Guess a number 1 - {range_max}: "))
+    else:
+        print("You did it!")
+        print("")
+        print(f"The number was {random_number}")
+        
+# Call function to run the game
+play_game()
