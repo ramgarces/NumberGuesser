271a16cca6b4a5d63b28390a1457c3e4c7f25994
Ramiro <ramgarces24@gmail.com>
2025-05-18 11:01:28 -0500

    Add comments for clarity and ideas

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # NumberGuesser
 Number Guesser project for learning Python
+
+This is my first Python project since I took courses on programming in college in 2021. I am also learning to use vim at the same time, so this should be fun!
--- numberGuesser.py
+++ numberGuesser.py
@@ -1,6 +1,18 @@
+# Ideas: Have user select number range for guessing, make sure number is staying the same for all guesses (not changing everytime a user enters a guess)
+
 import random
 
 # Generate a random number and assign to a variable
 random_number = random.randint(1, 10)
 
-print(random_number)
+# Get user's guess
+user_guess = int(input("Guess a number 1 - 10: "))
+
+# Does the user's guess match the random number?
+if user_guess == random_number:
+    print("You did it!")
+else:
+    print("Guess again.")
+
+print(f"Random number is: {random_number}")
+print(f"User number is: {user_guess}")
